5167591be890448bea18d03d4b614b1b19ba42cc
Clara-coder-49 <haduxinnuo@gmail.com>
2026-09-13 23:39:20 +0000

    missed class trying to catch up.

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,32 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
 
-
+favorite_number = 7
+print(favorite_number)
 
 # Problem 2
 # Create a variable for your height in meters and print it.
 
-
+Clara_h = 1.47
+print(Clara_h)
 
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
 
-
+animal = wolf
+print(f"My favorite animal is the {animal}.")
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
 
-
-
+first_name = Clara
+last_name = Harrison
+print(first_name)
+print(last_name)
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+age = 10
+favorite_color = blue
+print("My name is", first_name, "I am", age, "my favorite color is", favorite_color, ".")
